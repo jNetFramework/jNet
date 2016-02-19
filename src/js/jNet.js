2fa4feff9d7e39850ccfd0cca4666905c7a6cc91
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-02-19 11:09:55 +0300

    Events: + cleanSelection + getSelection -- Fixed example

--- src/js/jNet.js
+++ src/js/jNet.js
@@ -926,7 +926,12 @@ var jNet = new (function () {
                 document.selection.empty();
             }
             else {
-                window.getSelection().removeAllRanges();
+                if (typeof window.getSelection().empty == "function") {
+                    window.getSelection().empty();
+                }
+                else if (typeof window.getSelection().removeAllRanges == "function") {
+                    window.getSelection().removeAllRanges();
+                }
             }
             return this;
         };
