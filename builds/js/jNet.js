a89fcde5329bbe995152c1c46378710777adae82
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-02-20 01:46:16 +0300

    move jNCookie

--- builds/js/jNet.js
+++ builds/js/jNet.js
@@ -3,7 +3,7 @@
  *  @site https://babichev.net
  *  @year 2013 - 2016
  *  @version 0.482
- *  @build 1092
+ *  @build 1093
  */
 
 /**
@@ -75,122 +75,6 @@ var jNet = new (function () {
         return Object.keys(objData).map(function (key) {
             return encodeURIComponent(key) + '=' + encodeURIComponent(objData[key]);
         }).join('&');
-    };
-
-    /**
-     * @returns {*}
-     */
-    this.jNCookie = function () {
-
-        /**
-         * @type {Window.jNArray}
-         * @private
-         */
-        this._cookies = new jNArray();
-
-        /**
-         * @param key {string}
-         * @returns {boolean}
-         */
-        this.remove = function (key) {
-            this.set(key, '', {expires: -1});
-            return typeof this.get(key) == "undefined";
-        };
-
-        /**
-         * @param key {string}
-         * @param value {string}
-         * @param attributes {string}
-         * @returns {boolean}
-         */
-        this.set = function (key, value, attributes) {
-
-            if (typeof attributes == "undefined") {
-                /**
-                 * @type {{}}
-                 */
-                attributes = {};
-            }
-
-            if (typeof attributes.expires === 'number') {
-                /**
-                 * @type {Date}
-                 */
-                var expires = new Date();
-                expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
-
-                /**
-                 * @type {Date}
-                 */
-                attributes.expires = expires;
-            }
-
-            if (typeof attributes.path == "undefined") {
-                /**
-                 * @type {string}
-                 */
-                attributes.path = '/';
-            }
-
-            try {
-                var result = JSON.stringify(value);
-                if (/^[\{\[]/.test(result)) {
-                    value = result;
-                }
-            }
-            catch (e) {
-            }
-
-            /**
-             * @type {string}
-             */
-            value = encodeURIComponent(String(value))
-                .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-
-            /**
-             * @type {string}
-             */
-            key = encodeURIComponent(String(key));
-            key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
-            key = key.replace(/[\(\)]/g, encodeURI);
-
-            return (document.cookie = [
-                    key, '=', value,
-                    attributes.expires && '; expires=' + attributes.expires.toUTCString(),
-                    attributes.path && '; path=' + attributes.path,
-                    attributes.domain && '; domain=' + attributes.domain,
-                    attributes.secure ? '; secure' : ''
-                ].join('')).length > 0;
-
-        };
-
-        /**
-         * @param key
-         * @returns {*}
-         */
-        this.get = function (key) {
-            if (document.cookie.length) {
-                this._cookies._array = document.cookie.split('; ');
-                var rDecode = /(%[0-9A-Z]{2})+/g;
-                var obj = {};
-                this._cookies.forEach(function (line) {
-                    var parts = line.split('=');
-                    var name = parts[0].replace(rDecode, decodeURIComponent);
-                    var cookie = parts.slice(1).join('=');
-                    cookie = cookie.jNTrim('["]+');
-                    cookie = cookie.replace(rDecode, decodeURIComponent);
-                    obj[name] = cookie;
-                });
-                if (typeof key == "undefined") {
-                    return obj;
-                }
-                return obj[key];
-            }
-            return {};
-        };
-
-        return this;
-
     };
 
     /**
@@ -363,10 +247,123 @@ var jNet = new (function () {
 })();
 
 /**
+ * @returns {jNCookie}
+ */
+var jNCookie = function () {
+
+    /**
+     * @type {Window.jNArray}
+     * @private
+     */
+    this._cookies = new jNArray();
+
+    /**
+     * @param key {string}
+     * @returns {boolean}
+     */
+    this.remove = function (key) {
+        this.set(key, '', {expires: -1});
+        return typeof this.get(key) == "undefined";
+    };
+
+    /**
+     * @param key {string}
+     * @param value {string}
+     * @param attributes {{}}
+     * @returns {boolean}
+     */
+    this.set = function (key, value, attributes) {
+
+        attributes = attributes || {};
+
+        if (typeof attributes.expires === 'number') {
+            /**
+             * @type {Date}
+             */
+            var expires = new Date();
+            expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+
+            /**
+             * @type {Date}
+             */
+            attributes.expires = expires;
+        }
+
+        if (typeof attributes.path == "undefined") {
+            /**
+             * @type {string}
+             */
+            attributes.path = '/';
+        }
+
+        try {
+            var result = JSON.stringify(value);
+            if (/^[\{\[]/.test(result)) {
+                value = result;
+            }
+        }
+        catch (e) {
+        }
+
+        /**
+         * @type {string}
+         */
+        value = encodeURIComponent(String(value))
+            .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+
+        /**
+         * @type {string}
+         */
+        key = encodeURIComponent(String(key));
+        key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+        key = key.replace(/[\(\)]/g, encodeURI);
+
+        return (document.cookie = [
+                key, '=', value,
+                attributes.expires && '; expires=' + attributes.expires.toUTCString(),
+                attributes.path && '; path=' + attributes.path,
+                attributes.domain && '; domain=' + attributes.domain,
+                attributes.secure ? '; secure' : ''
+            ].join('')).length > 0;
+
+    };
+
+    /**
+     * @param key
+     * @returns {*}
+     */
+    this.get = function (key) {
+        if (document.cookie.length) {
+            this._cookies._array = document.cookie.split('; ');
+            var rDecode = /(%[0-9A-Z]{2})+/g;
+            var obj = {};
+            this._cookies.forEach(function (line) {
+                var parts = line.split('=');
+                var name = parts[0].replace(rDecode, decodeURIComponent);
+                var cookie = parts.slice(1).join('=');
+                cookie = cookie.jNTrim('["]+');
+                cookie = cookie.replace(rDecode, decodeURIComponent);
+                obj[name] = cookie;
+            });
+            if (typeof key == "undefined") {
+                return obj;
+            }
+            return obj[key];
+        }
+        return {};
+    };
+
+    return this;
+
+};
+
+/**
  * @param data {Array.<T>|undefined}
  * @returns {*}
  */
 var jNArray = function (data) {
+
+    'use strict';
 
     /**
      * @type {Array}
@@ -646,6 +643,8 @@ var jNArray = function (data) {
  * @returns {*}
  */
 var jNDocQuery = function (doc) {
+
+    'use strict';
 
     /**
      * @type {Window.jNArray}
--- src/js/jNet.js
+++ src/js/jNet.js
@@ -70,122 +70,6 @@ var jNet = new (function () {
     };
 
     /**
-     * @returns {*}
-     */
-    this.jNCookie = function () {
-
-        /**
-         * @type {Window.jNArray}
-         * @private
-         */
-        this._cookies = new jNArray();
-
-        /**
-         * @param key {string}
-         * @returns {boolean}
-         */
-        this.remove = function (key) {
-            this.set(key, '', {expires: -1});
-            return typeof this.get(key) == "undefined";
-        };
-
-        /**
-         * @param key {string}
-         * @param value {string}
-         * @param attributes {string}
-         * @returns {boolean}
-         */
-        this.set = function (key, value, attributes) {
-
-            if (typeof attributes == "undefined") {
-                /**
-                 * @type {{}}
-                 */
-                attributes = {};
-            }
-
-            if (typeof attributes.expires === 'number') {
-                /**
-                 * @type {Date}
-                 */
-                var expires = new Date();
-                expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
-
-                /**
-                 * @type {Date}
-                 */
-                attributes.expires = expires;
-            }
-
-            if (typeof attributes.path == "undefined") {
-                /**
-                 * @type {string}
-                 */
-                attributes.path = '/';
-            }
-
-            try {
-                var result = JSON.stringify(value);
-                if (/^[\{\[]/.test(result)) {
-                    value = result;
-                }
-            }
-            catch (e) {
-            }
-
-            /**
-             * @type {string}
-             */
-            value = encodeURIComponent(String(value))
-                .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-
-            /**
-             * @type {string}
-             */
-            key = encodeURIComponent(String(key));
-            key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
-            key = key.replace(/[\(\)]/g, encodeURI);
-
-            return (document.cookie = [
-                    key, '=', value,
-                    attributes.expires && '; expires=' + attributes.expires.toUTCString(),
-                    attributes.path && '; path=' + attributes.path,
-                    attributes.domain && '; domain=' + attributes.domain,
-                    attributes.secure ? '; secure' : ''
-                ].join('')).length > 0;
-
-        };
-
-        /**
-         * @param key
-         * @returns {*}
-         */
-        this.get = function (key) {
-            if (document.cookie.length) {
-                this._cookies._array = document.cookie.split('; ');
-                var rDecode = /(%[0-9A-Z]{2})+/g;
-                var obj = {};
-                this._cookies.forEach(function (line) {
-                    var parts = line.split('=');
-                    var name = parts[0].replace(rDecode, decodeURIComponent);
-                    var cookie = parts.slice(1).join('=');
-                    cookie = cookie.jNTrim('["]+');
-                    cookie = cookie.replace(rDecode, decodeURIComponent);
-                    obj[name] = cookie;
-                });
-                if (typeof key == "undefined") {
-                    return obj;
-                }
-                return obj[key];
-            }
-            return {};
-        };
-
-        return this;
-
-    };
-
-    /**
      * @returns {number}
      */
     this.now = function () {
@@ -355,10 +239,123 @@ var jNet = new (function () {
 })();
 
 /**
+ * @returns {jNCookie}
+ */
+var jNCookie = function () {
+
+    /**
+     * @type {Window.jNArray}
+     * @private
+     */
+    this._cookies = new jNArray();
+
+    /**
+     * @param key {string}
+     * @returns {boolean}
+     */
+    this.remove = function (key) {
+        this.set(key, '', {expires: -1});
+        return typeof this.get(key) == "undefined";
+    };
+
+    /**
+     * @param key {string}
+     * @param value {string}
+     * @param attributes {{}}
+     * @returns {boolean}
+     */
+    this.set = function (key, value, attributes) {
+
+        attributes = attributes || {};
+
+        if (typeof attributes.expires === 'number') {
+            /**
+             * @type {Date}
+             */
+            var expires = new Date();
+            expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
+
+            /**
+             * @type {Date}
+             */
+            attributes.expires = expires;
+        }
+
+        if (typeof attributes.path == "undefined") {
+            /**
+             * @type {string}
+             */
+            attributes.path = '/';
+        }
+
+        try {
+            var result = JSON.stringify(value);
+            if (/^[\{\[]/.test(result)) {
+                value = result;
+            }
+        }
+        catch (e) {
+        }
+
+        /**
+         * @type {string}
+         */
+        value = encodeURIComponent(String(value))
+            .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
+
+        /**
+         * @type {string}
+         */
+        key = encodeURIComponent(String(key));
+        key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
+        key = key.replace(/[\(\)]/g, encodeURI);
+
+        return (document.cookie = [
+                key, '=', value,
+                attributes.expires && '; expires=' + attributes.expires.toUTCString(),
+                attributes.path && '; path=' + attributes.path,
+                attributes.domain && '; domain=' + attributes.domain,
+                attributes.secure ? '; secure' : ''
+            ].join('')).length > 0;
+
+    };
+
+    /**
+     * @param key
+     * @returns {*}
+     */
+    this.get = function (key) {
+        if (document.cookie.length) {
+            this._cookies._array = document.cookie.split('; ');
+            var rDecode = /(%[0-9A-Z]{2})+/g;
+            var obj = {};
+            this._cookies.forEach(function (line) {
+                var parts = line.split('=');
+                var name = parts[0].replace(rDecode, decodeURIComponent);
+                var cookie = parts.slice(1).join('=');
+                cookie = cookie.jNTrim('["]+');
+                cookie = cookie.replace(rDecode, decodeURIComponent);
+                obj[name] = cookie;
+            });
+            if (typeof key == "undefined") {
+                return obj;
+            }
+            return obj[key];
+        }
+        return {};
+    };
+
+    return this;
+
+};
+
+/**
  * @param data {Array.<T>|undefined}
  * @returns {*}
  */
 var jNArray = function (data) {
+
+    'use strict';
 
     /**
      * @type {Array}
@@ -638,6 +635,8 @@ var jNArray = function (data) {
  * @returns {*}
  */
 var jNDocQuery = function (doc) {
+
+    'use strict';
 
     /**
      * @type {Window.jNArray}
