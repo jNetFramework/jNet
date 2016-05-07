775b74c8cb1384e329a6d07f1c87c817f9afd806
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-05-08 00:23:54 +0300

    old build

--- dist/jnet.v1.0.1.6.js
+++ dist/jnet.v1.0.1.6.js
@@ -3,7 +3,7 @@
  * @email <info@babichev.net> 
  * @project jNet 
  * @version 0.1.6 
- * @date 07-05-2016 
+ * @date 08-05-2016 
  **/
 jNetRegister(window, document, 'jNet', (function (document, fn) {
 
